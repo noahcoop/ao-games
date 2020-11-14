a8ae68c93ad8a840d04e7111c43b56333d159173
Noah Cooper <noahcoop@umich.edu>
2020-11-14 12:23:07 -0500

    initial parse of the board

--- client.py
+++ client.py
@@ -3,9 +3,43 @@
 import sys
 import json
 import socket
+import numpy as np
 
+NUM_ROWS = 6
+NUM_COLS = 7
+
+def check_empty(board):
+  """Checks if no moves have been made on the board"""
+  board = np.array(board)
+  return board.min() == 0
+
+def determine_valid_moves(board):
+  """Returns a list of up to 7 moves a player can make"""
+  valid_moves = []
+  for col in range(NUM_COLS):
+    if board[0][col] == 0:
+      for row in range(NUM_ROWS):
+        if board[row][col] != 0:
+          valid_moves.append([row - 1, col])
+          break
+  return valid_moves
+
+def get_optimal_move(player, board, valid_moves):
+  # If we can win, return the column that makes us win
+  # If opponent can win next turn, then try to block opponent
+  pass
+
+def is_winning_move(player, board, row, column):
+  pass
+    
 def get_move(player, board):
+  # TODO determine if board is empty
+  if check_empty(board):
+    return {"column": 3}
+
   # TODO determine valid moves
+  valid_moves = determine_valid_moves(board)
+
   # TODO determine best move
   return {"column": 1}
 
@@ -34,6 +68,6 @@ if __name__ == "__main__":
 
       move = get_move(player, board)
       response = prepare_response(move)
-      sock.sendall(response)
+      sock.sendall(response.encode('utf-8'))
   finally:
     sock.close()
--- test.py
+++ test.py
@@ -1,9 +1,19 @@
 import unittest
 import client
+import socket
+import json
 
 class TestPrepareResponse(unittest.TestCase):
   def test_prepare_response_returns_a_valid_response(self):
     self.assertEqual(client.prepare_response({"column": 1}), '{"column": 1}\n')
 
 if __name__ == '__main__':
+  init_test = {
+    'player': '1',
+    'maxTurnTime': 15000,
+    'board': [[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,0,0,0,0,0,0]]
+  }
+  sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+  sock.connect(('localhost', 8080))
+  sock.sendall(json.loads(init_test).encode('utf-8'))
   unittest.main()
