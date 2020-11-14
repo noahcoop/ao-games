41671acf2b3ae0384056d7a43c527098d05678fd
Noah Cooper <noahcoop@umich.edu>
2020-11-14 13:07:30 -0500

    test case for get_valid_moves

--- test.py
+++ test.py
@@ -3,17 +3,27 @@ import client
 import socket
 import json
 
+import unittest
+import client
+import socket
+import json
+
+empty = [[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,0,0,0,0,0,0]]
+four_turns = [[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[2,2,0,0,0,0,0],[1,1,0,0,0,0,0]]
+player_one_close = [[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[2,2,2,0,0,0,0],[1,1,1,0,0,0,0]]
+almost_full = [[1,2,1,2,2,1,0],[2,2,1,1,2,1,2],[1,2,2,1,1,2,1],[2,1,2,1,1,2,2],[2,2,2,0,0,0,0],[1,1,1,0,0,0,0]]
+
+
 class TestPrepareResponse(unittest.TestCase):
   def test_prepare_response_returns_a_valid_response(self):
     self.assertEqual(client.prepare_response({"column": 1}), '{"column": 1}\n')
 
+class TestValidMoves(unittest.TestCase):
+  def test_get_valid_moves(self):
+    self.assertEqual(len(client.determine_valid_moves(empty)), 7)
+    self.assertEqual(len(client.determine_valid_moves(almost_full)), 1)
+    self.assertEqual(len(client.determine_valid_moves(four_turns)), 7)
+    print(client.determine_valid_moves(four_turns))
+
 if __name__ == '__main__':
-  init_test = {
-    'player': '1',
-    'maxTurnTime': 15000,
-    'board': [[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,0,0,0,0,0,0]]
-  }
-  sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-  sock.connect(('localhost', 8080))
-  sock.sendall(json.loads(init_test).encode('utf-8'))
   unittest.main()
